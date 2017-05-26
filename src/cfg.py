6f5244a648d2571dabd7d12a3581dd52629eab15
giwyni <vsvconsult@gmail.com>
2017-05-26 13:34:41 -0700

    Axes titles

--- src/cfg.py
+++ src/cfg.py
@@ -8,39 +8,49 @@ cfg={'active':['exchange','Flying-A320s','meetup','github'] #['Flying-A320s','me
      ,'chDetails': {
          'bitcoin':{'pDu':{
                 "action":"rtm/subscribe","id":"20","body":{"channel":"bitcoin-transactions"}},'classNM':'bitcoin.bitcoin',
-                "charts":{"msgRate":{"Title":"Bitcoin msgs 10 second msg count"}}
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"Bitcoin msgs 10 second msg count"}}}
          }
          ,'meetup':{'pDu':{
                 "action":"rtm/subscribe","id":"20","body":{"channel":"Meetup-RSVP"}},'classNM':'default.default',
-                "charts":{"msgRate":{"Title":"Meetup msgs 10 second msg count"}}     
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"Meetup msgs 10 second msg count"}}}  
          }
          ,'github':{'pDu':{
                 "action":"rtm/subscribe","id":"20","body":{"channel":"github-events"}},'classNM':'default.default',
-                "charts":{"msgRate":{"Title":"Github events 10 second msg count"}}     
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"Github events 10 second msg count"}}}    
          }
          ,'Flying-A320s':{'pDu':{
                 "action":"rtm/subscribe","id":"20"
                 ,"body":{"channel":"air-traffic"
                          ,"filter":'select * where aircraft = "A320" and speed>200'
                                                     }},'classNM':'default.default',
-                "charts":{"msgRate":{"Title":"A320 air-traffic events 10 second msg count"}}     
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"A320 air-traffic events 10 second msg count"}}} 
          }
          ,'Bart':{'pDu':{
                 "action":"rtm/subscribe","id":"20","body":{"channel":"bartarrivalsschedule"}},'classNM':'default.default',
-                "charts":{"msgRate":{"Title":"BART events 10 second msg count"}}     
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"BART events 10 second msg count"}}}   
          }
         ,'twitter':{'pDu':{
                 "action":"rtm/subscribe","id":"20"
-                ,"body":{"channel":"Twitter-statuses-sample"
-                                                    }},'classNM':'default.default',
-                "charts":{"msgRate":{"Title":"Twitter events 10 second msg count"}}     
+                ,"body":{"channel":"Twitter-statuses-sample"}},'classNM':'default.default',
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"Twitter events 10 second msg count"}}}     
          }
         ,'exchange':{'pDu':{
                 "action":"rtm/subscribe","id":"20"
                 ,"body":{"channel":"exchange-rates"
                                                     }},'classNM':'bitcoin.exchange',
-                "charts":{"msgRate":{"Title":"Dollar exchange rate updated every 30 seconds"},"chartType":'column'}     
-         }                                            
+                "charts":{"msgRate":{
+                    "mixin":{"Title":"Dollar exchange rate updated every 30 seconds","chartType":'column',"haxis_title":"rate/$"}}}   
+         }                             
+    }
+     ,'websocketDetails': {
+         'endpoint':"wss://open-data.api.satori.com", 'appkey':"E0Acebca1fbB549AaBeA5D9bec2aacb8"
+        #urlString = endpoint + '?appkey='+appkey
     }
      ,'engines':{} #placeholder to store the processing engines (created from classNM) during run
 }
